a397a762319a3a6d7bc4f4e675e52d959c5da13f
Kedar Sawant <sawantkedar0803@gmail.com>
2026-04-28 13:58:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
 import mysql.connector
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__,
+            template_folder=os.path.join(base_dir, 'templates'),
+            static_folder=os.path.join(base_dir, 'static'))
 
 def get_db_connection():
     return mysql.connector.connect(
